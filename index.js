f87098d15df1458c5db081eebc7f35f66636c9bc
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-13 22:07:17 +0600

    add appointment to the database

--- index.js
+++ index.js
@@ -14,9 +14,24 @@ const port = 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mkcgo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+  
+
+  //insert data to the database
+  app.post('/addAppointment', (req, res) => {
+    const appointment = req.body;
+    appointmentCollection.insertOne(appointment)
+    .then(result => {
+        res.send(result.insertedCount > 0);
+    })
+  })
+
+  //load data from the database
+  app.get('/appointment', (req, res) => {
+
+  })
+
+
 });
 
 
@@ -25,4 +40,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port);
+app.listen(process.env.PORT || port);
